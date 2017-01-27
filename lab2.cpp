7907d0dc33cef72bd36d88c66685ca39ef003402
Mario <Maslz980outlook.com>
2017-01-27 15:02:32 -0600

    Terminando Ejercicio 2 y 3

--- lab2.cpp
+++ lab2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+#include <math.h>
 
 using std::cout;
 using std::cin;
@@ -23,6 +23,40 @@ int menu(int men){
 		cout<<"El angulo B es: "<<Ab<<endl;
 		cout<<"El Lado C es: "<<C<<endl;
 		cout<<"El angulo c es: "<<Ac<<endl;
+		break;
+		}
+		case 2:{
+		cout<<"Ingrese el coeficiente a"<<endl;
+		int a;
+		cin>>a;
+		cout<<"Ingrese el coeficiente b"<<endl;
+                int b;
+                cin>>b;
+		cout<<"Ingrese el coeficiente c"<<endl;
+                int c;
+                cin>>c;
+		int x1 = (-(b)+sqrt(b*b+(-4*a*c)))/2*a;
+                int x2 = (-(b)-sqrt(b*b+(-4*a*c)))/2*a;
+		cout<<"El resultado de la formula cuadratica usando + es: "<<x1<<endl;
+		cout<<"El resultado de la formula cuadratica usando - es: "<<x2<<endl;
+		break;
+		}
+		case 3:{
+		cout<<"Ingrese x1"<<endl;
+		int px0;
+		cin>>px0;
+		cout<<"Ingrese y1"<<endl;
+		int py0;
+		cin>>py0;
+                cout<<"Ingrese x2"<<endl;       
+                int px1;
+                cin>>px1;
+                cout<<"Ingrese y2"<<endl;       
+                int py1;
+                cin>>py1;
+		int d = sqrt((px1-px0)^2 + (py1 - py0)^2);
+		cout<<"La distancia es: "<<d<<endl;
+		break;
 		}
 	}
 }
